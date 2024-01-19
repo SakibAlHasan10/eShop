453767438451a6d470bdfbfc62dcb3dba3c1065c
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-19 11:40:30 +0600

    added pagesize components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,31 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+import PageSize from "./PageSize";
 
 const Navbar = () => {
-    return (
-        <div>
-            <div className='flex justify-between items-center'>
-        <div >
-          <h2>eShop</h2>
-          {/* <Image src={""} alt='logo'></Image> */}
+  return (
+    <div>
+      <PageSize>
+        <div className="flex justify-between items-center">
+          <div>
+            <h2>eShop</h2>
+            {/* <Image src={""} alt='logo'></Image> */}
+          </div>
+          <div>
+            <Link href={"/"}>
+              <button>Home</button>
+            </Link>
+            <Link href={"/saved"}>
+              <button>Saved</button>
+            </Link>
+            <Link href={"/electronics"}>
+              <button>Electronics</button>
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link href={"/"}>
-            <button>Home</button>
-          </Link>
-          <Link href={"/saved"}>
-            <button>Saved</button>
-          </Link>
-          <Link href={"/electronics"}>
-            <button>Electronics</button>
-          </Link>
-        </div>
-      </div>
-        </div>
-    );
+      </PageSize>
+    </div>
+  );
 };
 
 export default Navbar;
