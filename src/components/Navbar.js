45a1ac7d9518a1d2c88abba241471b6c82c68dbc
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-20 11:42:23 +0600

    added rounting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,26 @@
 import Link from "next/link";
 import React from "react";
 import PageSize from "./PageSize";
-
+const navs=["Home","Saved","Motors",
+"Electronics",
+"Collectibles",
+"Home & Garden",
+"Fashion",
+"Toys",
+"Sporting Goods",
+"Business & Industrial",
+"Jewelry & Watches",
+"eShop Live", "Refurbished" ]
 const Navbar = () => {
   return (
     <div>
       <PageSize>
-        <div className="my-3">
+        <div className="flex items-center justify-between text-sm my-4">
+          {
+            navs?.map(nav=><Link key={nav} href={`/${nav}`} className="hover:border-x px-2 border-black"><button>{nav}</button></Link>)
+          }
+        </div>
+        {/* <div className="my-3">
           <div className="flex items-center justify-between gap-5">
             <Link href={"/"}>
               <button>Home</button>
@@ -20,8 +34,17 @@ const Navbar = () => {
             <Link href={"/electronics"}>
               <button>Electronics</button>
             </Link>
+            <Link href={"/electronics"}>
+              <button>Collectibles</button>
+            </Link>
+            <Link href={"/electronics"}>
+              <button>Home & Garden</button>
+            </Link>
+            <Link href={"/electronics"}>
+              <button>Fashion</button>
+            </Link>
           </div>
-        </div>
+        </div> */}
       </PageSize>
     </div>
   );
